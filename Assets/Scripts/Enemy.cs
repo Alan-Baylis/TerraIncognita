4c20efe5b82941a47aab61ebe24d970937173f5c
Bomber100 <aleksej.tolkachov@gmail.com>
2019-03-01 23:33:16 +0200

    ready game v1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class Enemy : MonoBehaviour {
+    
+    public float enemyHealth = 50.0f;
+    
+    public void TakeDamage(float amount) {
+
+        enemyHealth -= amount;
+
+        if (enemyHealth <= 0f) {
+
+            Dead();
+
+        }
+    }
+
+    void Dead() {
+
+        gameObject.SetActive(false);
+
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
-
+using UnityEngine.UI;
 using UnityEngine;
+using System.Collections.Generic;
 
 
 public class PlayerController : MonoBehaviour {
@@ -11,25 +12,29 @@ public class PlayerController : MonoBehaviour {
     public float jumpheight = 3.0f;
     public static float playerHealth = 100;
     public static bool haveKey = false;
-
-   // public GameObject[] weapons;
-
+    public Text ammoText;
+    public GameObject[] weapons;
+    public int score;
+    public Dictionary<string, bool> keys = new Dictionary<string, bool>();
 
     private CharacterController _charCont;
+    private string keyNameTemplate = "Dungeon_Key_Set_";
                                        
     void Start() {
+        ammoText.gameObject.SetActive(false);
 
         _charCont = GetComponent<CharacterController>();
 
-      //  weapons[0].SetActive(false);
+       weapons[0].SetActive(false);
 
      //   weapons[1].SetActive(true);
-
       //  weapons[2].SetActive(false);
-
       //  weapons[3].SetActive(false);
 
-
+    //init keys array
+      for (int i = 0; i < 13; i++) {
+            keys.Add(keyNameTemplate + i, false);
+        }
     }
 
 
@@ -42,14 +47,11 @@ public class PlayerController : MonoBehaviour {
         movement = Vector3.ClampMagnitude(movement, speed);
 
         movement.y = gravity;
-          
         movement *= Time.deltaTime;
-
         movement = transform.TransformDirection(movement);
 
         _charCont.Move(movement);
-
-
+        
         if (Input.GetKeyDown(KeyCode.Space)) {
             Vector3 jump = new Vector3(0, jumpheight, 0);
             jump = Vector3.ClampMagnitude(jump, jumpheight);
@@ -62,10 +64,34 @@ public class PlayerController : MonoBehaviour {
 
 
     void OnTriggerEnter(Collider other) {
-
-        if (haveKey)
+        if (other.tag == "Wearpon") {
+            other.gameObject.SetActive(false);
+            weapons[0].SetActive(true);
+            ammoText.gameObject.SetActive(true);
+        }
+        else if (other.tag == "Energy") {
+            other.gameObject.SetActive(false);
+            score += 1;
+        }
+        else if (other.tag == "Key") {
+            print("GetKey: " + other.name);
+            keys[other.name] = true;
 
             other.gameObject.SetActive(false);
+            haveKey = true;
+        }
+        else if(other.tag == "Door") {
+            string doorIndex = other.name.Substring(5, other.name.Length - 5);
+
+           // print("door name " + doorIndex);
+           // print("have key for the door #" + doorIndex + " - " + keys[keyNameTemplate + doorIndex]);
+
+            if (keys[keyNameTemplate + doorIndex] == true) {
+               
+                other.gameObject.SetActive(false);
+            }
+        }
+        
 
     }
 }
